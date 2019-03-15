0c9deab59d2350db91146509aaa2705c3b140259
Daniel Lao <laodaniel@gmail.com>
2019-03-15 15:22:17 +0100

    Try to execute Pupeeter in Heroku

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require('express');
+const puppeteer = require('puppeteer');
+
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => res.send(`
-  <div id="app"></div>
-  <script>//console.error('Error during react reconciliation')</script>
-  <script id="preloaded-state">window.PRELOADED_STATE = {"application": {"locale": "fr_FR"}}</script>
-`)),
+app.get('/', async (req, res) => {
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+  const page = await browser.newPage();
+  page.on('console', msg => {
+    console.log(`console log ${msg._type}`);
+  });
+  await page.goto('http://m.weekendesk.fr');
+
+  await page.$('#app')
+  console.log('app exists');
+
+  const preloadedStateScript = await page.$eval('#preloaded-state', (element) => {
+    return element.innerHTML
+  })
+  const preloadedState = JSON.parse(
+    preloadedStateScript.substring(preloadedStateScript.indexOf('=') + 1),
+  );
+  console.log(`locale: ${preloadedState.application.locale}`);
+  browser.close();
+
+  res.send('ok');
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
