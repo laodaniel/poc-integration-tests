4eb469a9e08d3c2eb32c54b5ab2ff8d97dd76196
Daniel Lao <laodaniel@gmail.com>
2019-03-15 12:31:21 +0100

    Check if there is no JS error in the console

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send(`
+  <div id="app"></div>
+  <script>console.error('Error during react reconciliation')</script>
+`)),
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- test.spec.js
+++ test.spec.js
@@ -1,5 +1,7 @@
 const puppeteer = require('puppeteer');
 
+const HEROKU_UAT_APP_WEB_URL = 'http://localhost:8080';
+
 describe('Check mobile website is up', () => {
   it('should have id app in the dom', async() => {
     const browser = await puppeteer.launch({
@@ -7,9 +9,13 @@ describe('Check mobile website is up', () => {
     });
 
     const page = await browser.newPage();
+    page.on('console', msg => {
+      expect(msg._type).not.toEqual('error');
+    });
 
-    await page.goto('http://m.weekendesk.fr');
+    await page.goto(HEROKU_UAT_APP_WEB_URL);
     expect(await page.$('#app')).not.toBeNull();
     await browser.close();
   })
 })
+
