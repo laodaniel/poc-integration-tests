1b7cbd970bfac724629b66a1843cca12c7934303
Daniel Lao <laodaniel@gmail.com>
2019-03-15 12:42:06 +0100

    Split tests into small test case

--- test.spec.js
+++ test.spec.js
@@ -3,19 +3,23 @@ const puppeteer = require('puppeteer');
 const HEROKU_UAT_APP_WEB_URL = 'http://localhost:8080';
 
 describe('Check mobile website is up', () => {
-  it('should have id app in the dom', async() => {
-    const browser = await puppeteer.launch({
+  it('should go to mobile website without js error', async () => {
+    this.browser = await puppeteer.launch({
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
 
-    const page = await browser.newPage();
-    page.on('console', msg => {
+    this.page = await this.browser.newPage();
+    this.page.on('console', msg => {
       expect(msg._type).not.toEqual('error');
     });
+    await this.page.goto(HEROKU_UAT_APP_WEB_URL);
+  });
 
-    await page.goto(HEROKU_UAT_APP_WEB_URL);
-    expect(await page.$('#app')).not.toBeNull();
-    await browser.close();
-  })
-})
+  it('should have id app in the dom', async() => {
+    expect(await this.page.$('#app')).not.toBeNull();
+  });
 
+  it('should close the browser', async () => {
+    await this.browser.close();
+  });
+});
